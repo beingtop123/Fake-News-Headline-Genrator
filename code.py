8b0ef66cd961a1aa4126d7175ee25240ccabf841
Aman Gupta <ag1506986@example.com>
2025-06-24 10:49:26 +0530

    code

---
+++ code.py
@@ -0,0 +1,11 @@
+import random 
+lst=["hello","ji","kya","h","tu","ji","hyy","kyu"]
+lst1=["gya","hum","pe","kyooo"]
+lst2=["salman","bhai","kee"]
+
+x=random.choice(lst)
+y=random.choice(lst1)
+z=random.choice(lst2)
+print(f"{x} {y} {z}")
+
+ask=input("")
